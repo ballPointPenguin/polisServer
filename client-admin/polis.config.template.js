c04589ffae4796eadae6048d74c20b3e243d6c86
Benjamin Rosas <ben@aliencyb.org>
2020-05-31 17:22:12 -0500

    WIP

--- client-admin/polis.config.template.js
+++ client-admin/polis.config.template.js
@@ -5,6 +5,7 @@ module.exports = {
     "^192\\.168\\.1\\.140$",
     "^pol\\.is",
     ".+\\.pol\\.is$",
+    "^polisdemo\\.tech$",
     ".+\\.polisdemo\\.tech$",
     "^xip\\.io$",
     ".+\\.xip\\.io$",
